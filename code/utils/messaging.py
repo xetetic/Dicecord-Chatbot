e81eb06a85c229b795b617658a4e5c9e6c165cfe
xetetic <70036705+xetetic@users.noreply.github.com>
2020-11-26 13:10:40 -0600

    Update messaging.py

--- code/utils/messaging.py
+++ code/utils/messaging.py
@@ -17,20 +17,19 @@ goodDefault = ["You should take the beat, [userID].",
                "Don't let this luck go to waste, [userID].",
                "Pixies riding unicorns is right, [userID]!"]
 
-badDefault = ["Don't blame your bad luck on me, [userID]! I'm just a random number generator.",
-              "That was just a practice roll, right [userID]?",
+badDefault = ["That was just a practice roll, right [userID]?",
               "Ask for a dramatic failure [userID], you know you want to!",
               "[userID], I hope that wasn't an important roll...",
               "The dice gods are displeased, [userID].",
               "I hope Gaius didn't see that, [userID]...",
-              "You didn't really *need* to succeed on that roll anyway, [userID].",
               "[userID], that SUCKS.",
               "I see sunshine and rainbows in your future, [userID]...",
               "[userID], maybe you should have been kept back as a fledgling for another year...",
               "[userID], why did your Sire let you out again?",
-              "You're on fire tonight, [userID].",
+              "You're on fire tonight, [userID]. Positively BURNING UP.",
               "Gaius *definitely* saw that, [userID].",
-              "The racoons like you because you're trash, [userID]."]
+              "The racoons like you because you're trash, [userID].",
+              "You're a perfect candidate to join the Corcle, [userID]."]
 
 frenzyDefault = ["Bloody hell, [userID]!"]
 
@@ -40,25 +39,30 @@ natalieDefault = ["[userID], you should be shot in the head. Again.",
                   "[userID], this is why you need to be dragged home.",
                   "I bet you didn't *remember* you were this bad, [userID].",
                   "The Meow Mix song is deafening, [userID].",
-                  "You should have worn jeans, [userID]."]
+                  "You should have worn jeans, [userID].",
+                 "Somewhere in your head, VKR is laughing at your failure [userID]."]
 
 jackDefault = ["This is why Armand could never *really* care, [userID].",
-               "This is why Erik doesn't talk to you, [userID].",
+               "This is why Erik didn't talk to you for four years, [userID].",
                "[userID], Gaius thinks you haven't learned your lesson yet...",
                "Ami is rolling in her grave, [userID]. If she wasn't dead she'd die of disappointment.",
                "Do we need to call a security guard, [userID]?",
                "Don't stain the carpet, [userID]!",
                "Do you need a new shirt, [userID]?",
-               "Time to get the tarp, [userID]."]
+               "Time to get the tarp, [userID].",
+               "Don't make me shoot you in the head with your own gun again, [userID]."]
 
 teddieDefault = ["Go jump in a lake, [userID].",
-                 "Bless your heart, [userID]."]
+                 "Bless your heart, [userID].",
+                "Aww, do we need to leave Tom's arm behind again so you'll feel better for your failure, [userID]?"]
 
 jackGood = ["Ami would be proud, [userID]!",
             "That's some *damn* good curry.",
             "I told you: You've got this, [userID].",
             "You're a credit to your family's bloodline, [userID].",
-            "You're too sexy for your shirt, [uesrID]!"]
+            "You're too sexy for your shirt, [userID]!",
+            "Even Matilda would give you a gold star for this, [userID].",
+            "You're getting a brand new Armani shirt for this, [userID]."]
 
 natGood = ["Matilda would be proud, [userID]!",
            "That's the girl David loves: [uesrID]!",
@@ -67,7 +71,10 @@ natGood = ["Matilda would be proud, [userID]!",
 
 tedGood = ["You're so good, you hacked reality, [userID].",
            "This is the Matrix and you are the One, [userID]!",
-           "They were right: Your success is positively demonic, [userID]!"]
+           "They were right: Your success is positively demonic, [userID]!",
+          "Does this feel just as good as when you'll shoot the shit out of Jane, [userID]?",
+          "Other percentages may vary, but you're always 100% AWESOME to us, [userID].",
+          "If you can survive the south, you can survive the Corcle, [userID]!"]
 
 badParadox = ["The abyss does not react kindly to your manipulation, [userID].",
               "Let's make things interesting [userID], go for a manifestation!",
